2fbadbf56ec542d14a8bcf292005d9851482d40c
Ajay Kumar <ajaybunty91@gmail.com>
2023-04-12 00:08:16 +0530

    Middleware

--- index.js
+++ index.js
@@ -22,9 +22,13 @@ const app = express();
 app.use(express.json({limit: "10mb"}));
 app.use(morgan("common"));
 app.use(cookieParser());
-app.use(cors({ 
+let origin = 'http://localhost:3000'
+if(process.env.NODE_ENV === "production"){
+    origin = process.env.CLIENT_DOMAIN;
+  }
+app.use(cors({
     credentials: true,
-    origin: process.env.CLIENT_DOMAIN
+    origin
 }))
 
 app.use('/auth', authRouter);
